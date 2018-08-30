f8d8fc43cab5e066dc62ceed0760f96cdf40d159
Vision Systems GmbH <contact@visionsystems.de>
2018-08-30 10:39:47 +0200

    CAN: add C examples

---
+++ CAN/c/README.md
@@ -0,0 +1,31 @@
+C Examples for VSCAN API
+========================
+
+Requirements
+------------
+
+These examples come with a CMake file `CMakeLists.txt`. CMake usage is optional.
+
+You'll also need vs_can_api library files. Either put them into this folder
+or anywhere where CMake or the IDE of your choice will be able to find them.
+
+Compilation
+-----------
+
+### Linux
+
+1  `git clone https://github.com/visionsystemsgmbh/programming_examples.git`
+2. `cd CAN/c`
+3. `mkdir build`
+4. `cd build`
+5. `cmake ..`
+6. `make`
+
+### Windows (Microsoft Visual Studio 2008)
+
+1  `git clone https://github.com/visionsystemsgmbh/programming_examples.git`
+2. `cd CAN/c`
+3. `mkdir build`
+4. `cd build`
+5. `cmake .. -G "Visual Studio 9 2008"`
+6. open generated solution file
---
+++ CAN/c/vscansend.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#if defined(__unix__)
+#include <unistd.h>
+#endif
+
+#include "vs_can_api.h"
+
+#define GOTO_ERROR(fmt, ...) do { \
+		printf(fmt "\n", ##__VA_ARGS__); \
+		goto error; \
+	} while(0)
+
+int main (int argc, char *argv[])
+{
+	char *tty;
+	VSCAN_MSG msg;
+	VSCAN_HANDLE h = -1;
+	VSCAN_API_VERSION version;
+	DWORD rv;
+	DWORD bitrate;
+
+	if (VSCAN_Ioctl(0, VSCAN_IOCTL_GET_API_VERSION, &version))
+		GOTO_ERROR("Failed to get API version");
+	
+	printf("VSCAN-API Version %d.%d.%d\n", version.Major, version.Minor, version.SubMinor);
+
+	if (argc != 3) 
+		GOTO_ERROR("Usage: vscansend.exe <CAN-Channel> <CAN-Bitrate>\n"
+			   "Example: vscansend.exe COM3 S2");
+
+	tty = argv[1];
+
+	/* handle bitrate parameter */
+	if (strlen(argv[2]) > 8)
+		GOTO_ERROR("CAN bitrate must look like 'Sxxx', where x can be an index\n"
+			   "from 1 to 8 or bitrate as a number like 125000");
+	if (sscanf(argv[2], "S%7d", &bitrate) != 1)
+		GOTO_ERROR("Failed to parse CAN bitrate");
+	if (bitrate < 1 || bitrate > 1000000)
+		GOTO_ERROR("Wrong bitrate %d\n", bitrate);
+
+	h = VSCAN_Open(tty, VSCAN_MODE_NORMAL);
+	if (h <= 0)
+		GOTO_ERROR("VSCAN_Open failed for %s with %d", tty, h);
+	if (VSCAN_Ioctl(h, VSCAN_IOCTL_SET_SPEED, (void*)bitrate) != VSCAN_ERR_OK)
+		GOTO_ERROR("Setting CAN speed failed");
+
+	msg.Id = 0x100;
+	msg.Size = 4;
+	msg.Data[0] = 0x00;
+	msg.Data[1] = 0x01;
+	msg.Data[2] = 0x02;
+	msg.Data[3] = 0x03;
+
+	if (VSCAN_Write(h, &msg, 1, &rv) != VSCAN_ERR_OK)
+		GOTO_ERROR("VSCAN_Write failed");
+	if (VSCAN_Flush(h) != VSCAN_ERR_OK)
+		GOTO_ERROR("VSCAN_Flush failed");
+
+#if defined(__unix__)
+	sleep(1);
+#else
+	Sleep(1000);
+#endif
+
+	VSCAN_Close(h);
+	return 0;
+error:
+	if (h > 0)
+		VSCAN_Close(h);
+
+	return -1;
+}
