be9979eb2a00f65718021c70383e4bd5661c2ff2
Vision Systems GmbH <contact@visionsystems.de>
2019-09-24 11:45:08 +0200

    COM: add dotnet example

---
+++ COM-ports/c.sharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+
+namespace c.sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort ser = new SerialPort("COM5", 115200);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
