32c75fdfe0bcff405f6ae725af4bcae171b6d9ee
Vision Systems GmbH <contact@visionsystems.de>
2019-11-19 12:17:53 +0100

    CAN: add CANopen example

--- CAN/python/README.md
+++ CAN/python/README.md
@@ -24,3 +24,21 @@ Replace `CAN_DEV` variable with required device name:
 
 * `COM32@3000000` for USB-CAN Plus device
 * `socket://192.168.254.254:2001` for NetCAN Plus device
+
+CANopen Example
+---------------
+
+CANopen example uses
+[canopen](https://github.com/christiansandberg/canopen)
+package that uses python-can package for CAN communication. Hence the
+same device parameters also apply to CANopen.
+
+    pip install canopen
+
+This example shows how a CANopen master can access a CANopen node and
+read/modify its variables. You will need an ESD file describing your
+node, and a path to this file must be passed to the RemoteNode
+constructor.
+
+This example was tested against a CANopen node from
+[CANopenSocket](https://github.com/CANopenNode/CANopenSocket) project.
---
+++ CAN/python/vscanopen.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+"""
+Example for reading/writing CANopen objects.
+"""
+
+import canopen
+
+CAN_DEV = '/dev/ttyUSB0@3000000'
+
+
+def main():
+    """main routine."""
+
+    print("Starting CANopen master")
+    try:
+        network = canopen.Network()
+        network.connect(bustype='slcan',
+                        channel=CAN_DEV,
+                        rtscts=True,
+                        bitrate=1000000)
+        network.check()
+
+        # create and add a remote node using an EDS file
+        node = canopen.RemoteNode(4, '/path/to/object_dictionary.eds')
+        network.add_node(node)
+
+        # read a variable using SDO
+        val = node.sdo[0x1017].raw
+        print(val)
+
+        # change a variable using SDO
+        node.sdo[0x1017].raw = 4000
+
+        # reread the modified variable
+        val = node.sdo[0x1017].raw
+        print(val)
+
+    except Exception as err:
+        print("Error occurred: {}".format(err))
+    finally:
+        network.sync.stop()
+        network.disconnect()
+
+
+if __name__ == '__main__':
+    main()
